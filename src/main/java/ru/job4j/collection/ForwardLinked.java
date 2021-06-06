5c32082fce974b8f7cf1e66211cf2638500b6efd
vnovopashin <novopashinva@gmail.com>
2021-06-06 11:28:42 +0700

    6. Перевернуть связанный список [#161]

--- src/main/java/ru/job4j/collection/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/ForwardLinked.java
@@ -61,6 +61,27 @@ public class ForwardLinked<T> implements Iterable<T> {
         return temp.value;
     }
 
+    /**
+     * Метод переставляет элементы в обратном порядке,
+     * т.е. переворачивает связный список
+     *
+     * @return возвращает true если элементы успешно переставлены
+     */
+    public boolean revert() {
+        if (!(head == null) && head.next != null) {
+            Node<T> current = head.next;
+            head.next = null;
+            while (current != null) {
+                Node<T> next = current.next;
+                current.next = head;
+                head = current;
+                current = next;
+            }
+            return true;
+        }
+        return false;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<>() {
