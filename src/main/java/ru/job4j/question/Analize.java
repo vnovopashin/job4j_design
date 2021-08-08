57b2e763200198ac017c85661482c4e1b3856443
vnovopashin <novopashinva@gmail.com>
2021-08-08 20:50:31 +0700

    2. Статистика по коллекции. [#45889 #178822]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -27,29 +27,23 @@ public class Analize {
      */
     public static Info diff(Set<User> previous, Set<User> current) {
 
-        Map<Integer, String> previousMap = new HashMap<>();
-        Map<Integer, String> currentMap = new HashMap<>();
         Info info = new Info(0, 0, 0);
+        Map<Integer, String> map = new HashMap<>();
 
         for (User user : previous) {
-            previousMap.put(user.getId(), user.getName());
+            map.put(user.getId(), user.getName());
         }
+
         for (User user : current) {
-            currentMap.put(user.getId(), user.getName());
-        }
-        for (Integer key : currentMap.keySet()) {
-            if (!previousMap.containsKey(key)) {
+            if (map.containsKey(user.getId())) {
+                if (!map.containsValue(user.getName())) {
+                    info.setChanged(info.getChanged() + 1);
+                }
+            } else {
                 info.setAdded(info.getAdded() + 1);
             }
         }
-        for (Map.Entry<Integer, String> entry : previousMap.entrySet()) {
-            if (currentMap.containsKey(entry.getKey()) && !currentMap.containsValue(entry.getValue())) {
-                info.setChanged(info.getChanged() + 1);
-            }
-            if (!currentMap.containsKey(entry.getKey())) {
-                info.setDeleted(info.getDeleted() + 1);
-            }
-        }
+        info.setDeleted(previous.size() + info.getAdded() - current.size());
         return info;
     }
 }
