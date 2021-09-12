a586f8dc27d59ff5d9e676dd1f52e4999b26b933
vnovopashin <novopashinva@gmail.com>
2021-09-12 20:38:26 +0700

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -11,10 +11,12 @@ import java.net.Socket;
  * Класс реализует программу для обмена данными
  * с использованием протокола TCP/IP, данный протокол имеет возможность
  * передать данные и подтвердить, что они успешно дошли до получателя.
- * В качестве клиента используется программа cURL,
+ * В качестве клиента используется программа cURL (так же можно использовать браузер),
  * сервером является данный класс, он реализован посредством
  * java.net.Socket и позволяет получить сообщение и передать его.
- * При выполнении запроса http://localhost:9000/?msg=Bye, сервер завершает свою работу.
+ * При выполнении запроса http://localhost:9000/?msg=Hello, сервер отвечает "Hello",
+ * в случае любого другого запроса, например http://localhost:9000/?msg=Java отвечает "What?",
+ * в случае запроса http://localhost:9000/?msg=Bye, сервер завершает свою работу.
  *
  * @author Vasiliy Novopashin
  * @version 1.0
@@ -28,13 +30,20 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    StringBuilder sb = new StringBuilder();
+
                     for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
-                        if (str.equalsIgnoreCase("GET /?msg=Bye HTTP/1.1")) {
-                            server.close();
-                            break;
-                        }
-                        System.out.println(str);
+                        sb.append(str);
                     }
+                    if (sb.toString().contains("GET /?msg=Hello HTTP/1.1")) {
+                        out.write("Hello\r\n\r\n".getBytes());
+                    } else if (sb.toString().contains("GET /?msg=Exit HTTP/1.1")) {
+                        server.close();
+                        break;
+                    } else {
+                        out.write("What?\r\n\r\n".getBytes());
+                    }
+                    System.out.println(sb);
                     out.flush();
                 }
             }
