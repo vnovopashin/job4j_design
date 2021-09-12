e8ee708a53b2bebb5328d7363c080d84a7746538
vnovopashin <novopashinva@gmail.com>
2021-09-12 16:27:50 +0700

    2. Simple Loggin Facade 4 Java. [#268849]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,10 +1,16 @@
 package ru.job4j.io;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Пример логирования с использованием slf4j
+ *
+ * @author Vasiliy Novopashin
+ * @version 1.0
+ */
 public class UsageLog4j {
-    private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
+    private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
         LOG.trace("trace message");
